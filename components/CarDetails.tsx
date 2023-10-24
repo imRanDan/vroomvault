2f35ac935a2b75bdd50639b72b0a301c254a840a
Danyal Imran <dan.imran97@gmail.com>
2023-10-24 13:49:49 -0400

    working on carcard details

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
+import { generateCarImageUrl } from '@/utils';
+
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
@@ -63,15 +65,19 @@ const CarDetails = ({ isOpen, closeModal, car}:
               <div className="flex-1 flex flex-col gap-3">
                 <div className="relative w-full h-40 bg-pattern bg-cover
                 bg-center rounded-lg">
-                  <Image src="/hero.png" alt="car model" 
+                  <Image src={generateCarImageUrl(car, 'angle')} alt="car model" 
                   fill priority className="object-contain" />
                 </div>
 
                 <div className="flex gap-3">
-                  <div className="flex-1 relative w-full h-24 
-                  bg-primary-blue-100 rounded-lg">
-                    <Image src="/hero.png" alt="car model" 
-                    fill priority className="object-contain" />
+                  <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                    <Image src={generateCarImageUrl(car, 'angle')} alt="car model" fill priority className="object-contain" />
+                  </div>
+                  <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                    <Image src={generateCarImageUrl(car, 'angle')} alt="car model" fill priority className="object-contain" />
+                  </div>
+                  <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                    <Image src={generateCarImageUrl(car, 'angle')} alt="car model" fill priority className="object-contain" />
                   </div>
                 </div>
               </div>
